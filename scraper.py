cbda1d328388b51153d73cf2e230e07829e5f057
Carter Williams <carterpatwill@gmail.com>
2026-06-20 18:34:27 -1000

    login functionality with env username and password

--- scraper.py
+++ scraper.py
@@ -1,13 +1,24 @@
 import asyncio
+import os
+from dotenv import load_dotenv
 from playwright.async_api import async_playwright
+
+load_dotenv()
 
 
 async def run():
+    email = os.environ.get("NEXTDOOR_EMAIL", "")
+    password = os.environ.get("NEXTDOOR_PASSWORD", "")
+
     async with async_playwright() as p:
         browser = await p.chromium.launch(headless=False)
         page = await browser.new_page()
-        await page.goto("https://nextdoor.com")
-        await page.click('[data-testid="header-log-in"]')
+        await page.goto("https://nextdoor.com/login/?next=%2F")
+
+        await page.fill('[data-testid="email-address-input"]', email)
+        await page.fill('[data-testid="password-input"]', password)
+        await page.click('[data-testid="signin_button"]')
+
         input("Press Enter to close...")
         await browser.close()
 
